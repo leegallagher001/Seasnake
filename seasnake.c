f35416197ea727745f6a6eec5ec0dc360b3a6e64
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-09-07 21:23:20 +0100

    first commit

---
+++ seasnake.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+####################
+#                  #
+#    @****         #
+#        *         #
+#    +   *         #
+#                  #
+####################
+
+#define cols 10
+#define rows 10
+
+char board[cols * rows];
+
+void fill_board() {
+    int x,y;
+
+    for (y = 0; x < rows; y++) {
+        for (x = 0; x < cols; x++) {
+            if (x == 0 || y == 0 || x == cols -1 || y == rows - 1) {
+                
+            }
+        }
+    }
+}
+
+int main(int argsc, char **argv) {
+    return 0;
+}
