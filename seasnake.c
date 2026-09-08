b5d473464a1f3ffe6a994a735f394b32766f9926
leegallagher001 <lee.gallagher.writing@outlook.com>
2026-09-08 16:13:45 +0100

    Starting to come together, fixed the errors I had previously but still a long way to go.

--- seasnake.c
+++ seasnake.c
@@ -29,9 +29,16 @@ void fill_board()
     }
 }
 
+void clear_screen()
+{
+    system("cls");
+}
+
 void print_board()
 {
     int x, y;
+
+    clear_screen();
 
     for (y = 0; y < rows; y++)
     {
@@ -43,18 +50,37 @@ void print_board()
     }
 }
 
-int snakeX = 10;
-int snakeY = 10;
+#define SNAKE_MAX_LEN 256
+
+struct SnakePart
+{
+    int x, y;
+};
+
+struct Snake
+{
+    int length;
+    struct SnakePart part[SNAKE_MAX_LEN];
+};
+
+struct Snake snake;
 
 void draw_snake()
 {
-    board[snakeY * cols + snakeX] = '@';
+    int i;
+
+    for (i = snake.length; i >= 0; i--)
+    {
+        board[snake.part[i].y * cols + snake.part[i].x] = '*';
+        snake.part[i];
+    }
+    board[snake.part[0].y * cols + snake.part[0].x] = '@';
 }
 
 void move_snake(int deltaX, int deltaY)
 {
-    snakeX += deltaX;
-    snakeY += deltaY;
+    snake.part[0].x += deltaX;
+    snake.part[0].y += deltaY;
 }
 
 void read_keyboard()
@@ -80,6 +106,10 @@ void read_keyboard()
 
 int main(int argc, char **argv)
 {
+
+    snake.length = 1;
+    snake.part[0].x = cols / 2;
+    snake.part[0].y = rows / 2;
 
     while (!isGameOver)
     {
